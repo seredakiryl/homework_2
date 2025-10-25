9bd1a3e9979fefa5bcd49d34d49c5024eed5e2e2
kirillsereda <seredaKiryl@gmail.com>
2025-10-25 15:45:08 +0300

    test

--- src/blogs/validation/blog.input-dto.validation-middlewares.ts
+++ src/blogs/validation/blog.input-dto.validation-middlewares.ts
@@ -1,39 +1,39 @@
-import {body} from "express-validator";
+import { body } from 'express-validator';
 
-const nameValidation = body('data.attributes.name')
-    .isString()
-    .withMessage('name should be string')
-    .trim()
-    .isLength({min: 1, max: 15})
-    .withMessage('Length of name is not correct');
+const nameValidation = body('name')
+  .isString()
+  .withMessage('name should be string')
+  .trim()
+  .isLength({ min: 1, max: 15 })
+  .withMessage('Length of name is not correct');
 
 
-const descriptionValidation = body('data.attributes.description')
-    .isString()
-    .withMessage('description should be string')
-    .trim()
-    .isLength({min: 1, max: 500})
-    .withMessage('Length of description is not correct');
+const descriptionValidation = body('description')
+  .isString()
+  .withMessage('description should be string')
+  .trim()
+  .isLength({ min: 1, max: 500 })
+  .withMessage('Length of description is not correct');
 
-const websiteUrlValidation = body('data.attributes.websiteUrl')
-    .isString()
-    .withMessage('websiteUrl should be string')
-    .trim()
-    .isLength({min: 1, max: 100})
-    .withMessage('Length of websiteUrl is not correct')
-    .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
-    .withMessage('need valid pattern /^https:\\/\\/([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$/)')
+const websiteUrlValidation = body('websiteUrl')
+  .isString()
+  .withMessage('websiteUrl should be string')
+  .trim()
+  .isLength({ min: 1, max: 100 })
+  .withMessage('Length of websiteUrl is not correct')
+  .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
+  .withMessage('need valid pattern /^https:\\/\\/([a-zA-Z0-9_-]+\\.)+[a-zA-Z0-9_-]+(\\/[a-zA-Z0-9_-]+)*\\/?$/)');
 
 export const blogCreateInputValidation = [
-    nameValidation,
-    descriptionValidation,
-    websiteUrlValidation
+  nameValidation,
+  descriptionValidation,
+  websiteUrlValidation,
 ];
 
 export const blogUpdateInputValidation = [
-    nameValidation,
-    descriptionValidation,
-    websiteUrlValidation
+  nameValidation,
+  descriptionValidation,
+  websiteUrlValidation,
 ];
 
 
