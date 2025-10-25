bb167ab0c90621c4c2f5e4b14131e79d16c2a8c6
kirillsereda <seredaKiryl@gmail.com>
2025-10-25 16:24:13 +0300

    fix_updatePost

--- src/posts/routers/handlers/update-post.handler.ts
+++ src/posts/routers/handlers/update-post.handler.ts
@@ -1,18 +1,19 @@
-import {Request, Response} from 'express';
-import {postsRepository} from "../../repositories/posts.repository";
-import {HttpStatus} from "../../../core/types/http-statuses";
-import {createErrorMessages} from "../../../core/utils/error.utils";
-import {PostInputDto} from "../../dto/post.input-dto";
+import { Request, Response } from 'express';
+import { postsRepository } from '../../repositories/posts.repository';
+import { HttpStatus } from '../../../core/types/http-statuses';
+import { createErrorMessages } from '../../../core/utils/error.utils';
+import { PostInputDto } from '../../dto/post.input-dto';
 
 export const updatedPostHandler = (req: Request<{ id: string }, {}, PostInputDto>, res: Response) => {
-    const id = req.params.id;
-    const post = postsRepository.findById(id);
+  const id = req.params.id;
+  const post = postsRepository.findById(id);
+  const newPost = req.body;
 
-    if (!post) {
-        res.status(HttpStatus.NotFound).send(createErrorMessages([{field: 'id', message: "No post found"}]))
-        return;
-    }
+  if (!post) {
+    res.status(HttpStatus.NotFound).send(createErrorMessages([{ field: 'id', message: 'No post found' }]));
+    return;
+  }
 
-    postsRepository.update(id, post)
-    res.sendStatus(HttpStatus.NoContent)
-}
+  postsRepository.update(id, newPost);
+  res.sendStatus(HttpStatus.NoContent);
+};
