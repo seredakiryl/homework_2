334301c875f1537ecc343e4f85eaa53749e8d7aa
kirillsereda <seredaKiryl@gmail.com>
2025-10-25 15:31:12 +0300

    fix

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,27 +1,27 @@
-import express, {Express, Request, Response} from "express";
+import express, { Express, Request, Response } from 'express';
 
-import {BLOGS_PATH, POSTS_PATH} from "./core/paths/paths";
-import {blogsRouter} from "./blogs/routers/blogs.router";
-import {postsRouter} from "./posts/routers/posts.router";
-import {db} from "./db/in-memory.db";
-import {HttpStatus} from "./core/types/http-statuses";
+import { BLOGS_PATH, POSTS_PATH } from './core/paths/paths';
+import { blogsRouter } from './blogs/routers/blogs.router';
+import { postsRouter } from './posts/routers/posts.router';
+import { db } from './db/in-memory.db';
+import { HttpStatus } from './core/types/http-statuses';
 
 export const setupApp = (app: Express) => {
-    app.use(express.json());
+  app.use(express.json());
 
 
-    app.get('/', (req, res) => {
-        res.status(HttpStatus.Ok).send('hello world!!!');
-    });
+  app.get('/', (req, res) => {
+    res.status(HttpStatus.Ok).send('hello world!!!');
+  });
 
-    app.use(BLOGS_PATH, blogsRouter);
-    app.use(POSTS_PATH, postsRouter);
+  app.use(BLOGS_PATH, blogsRouter);
+  app.use(POSTS_PATH, postsRouter);
 
-    app.delete('/testing/all-data', (req: Request, res: Response) => {
-            db.blogs = []
-            db.posts = []
-            res.status(HttpStatus.NoContent).send('success response!');
-        }
-    )
-    return app;
+  app.delete('/testing/all-data', (req: Request, res: Response) => {
+      db.blogs = [];
+      db.posts = [];
+      res.status(HttpStatus.NoContent).send('All data is deleted');
+    },
+  );
+  return app;
 };
