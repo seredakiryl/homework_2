5f747df1b7594dfcd6c3c605c02063be3f7af4b3
kirillsereda <seredaKiryl@gmail.com>
2025-11-03 09:59:01 +0300

    fix_idValid

--- src/core/middlewares/validation/params-id.validation-middleware.ts
+++ src/core/middlewares/validation/params-id.validation-middleware.ts
@@ -1,11 +1,9 @@
-import {param} from 'express-validator';
+import { param } from 'express-validator';
 
 export const idValidation = param('id')
-    .exists()
-    .withMessage('ID is required')
-    .isString()
-    .withMessage('ID must be a string')
-    .isLength({min: 1})
-    .withMessage('ID must not be empty')
-    .isNumeric()
-    .withMessage('ID must be a numeric string');
+  .exists()
+  .withMessage('ID is required')
+  .isString()
+  .withMessage('ID must be a string')
+  .isMongoId()
+  .withMessage('Неверный формат ObjectId');
